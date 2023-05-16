667c6bd4ef270c3fa9388c57371191dca124d984
lenixlobo <lenixlobo@gmail.com>
2023-05-16 21:43:57 +0530

    initial test run

---
+++ Synthesizable/UART_RX/UART_RX.ip_user_files/bd/design_1/sim/design_1.vhd
@@ -0,0 +1,9 @@
+--Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+--Date        : Sun May 14 12:44:34 2023
+--Host        : LAPTOP-8KCDPL3U running 64-bit major release  (build 9200)
+--Command     : generate_target design_1.bd
+--Design      : design_1
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
